009f9501e5f650ea1e8d32719c3ba797389d986b
Claromes <claromes@hey.com>
2024-04-27 18:22:57 -0300

    add filters

--- api/resources/medical_care.py
+++ api/resources/medical_care.py
@@ -1,6 +1,5 @@
 from flask import request, jsonify
 from flask_restful import Resource
-import csv
 
 from models.medical_care import MedicalCare
 
@@ -8,6 +7,35 @@ from models.medical_care import MedicalCare
 class MedicalCareList(Resource):
 
     def get(self):
-        medical_care = MedicalCare.query.all()
+        medical_care_query = MedicalCare.query
+
+        date_filter = request.args.get("data_atendimento")
+        health_condition_filter = request.args.get("condicao_saude")
+        unit_filter = request.args.get("unidade")
+
+        if date_filter:
+            medical_care_query = medical_care_query.filter(
+                MedicalCare.medical_care_date == date_filter)
+        if health_condition_filter:
+            if health_condition_filter == "hipertensao":
+                medical_care_query = medical_care_query.filter(
+                    MedicalCare.health_condition == "hipertensao")
+            elif health_condition_filter == "diabetes":
+                medical_care_query = medical_care_query.filter(
+                    MedicalCare.health_condition == "diabetes")
+            elif health_condition_filter == "ferida_vascular":
+                medical_care_query = medical_care_query.filter(
+                    MedicalCare.health_condition == "ferida vascular")
+            elif health_condition_filter == "dengue":
+                medical_care_query = medical_care_query.filter(
+                    MedicalCare.health_condition == "dengue")
+            elif health_condition_filter == "tuberculose":
+                medical_care_query = medical_care_query.filter(
+                    MedicalCare.health_condition == "tuberculose")
+        if unit_filter:
+            medical_care_query = medical_care_query.filter(
+                MedicalCare.unit == unit_filter)
+
+        medical_care = medical_care_query.all()
 
         return jsonify(results=medical_care)
